12055e67e585bb26b3c56428197f40e879e1b334
Chris Hairfield <chairfield@gmail.com>
2015-11-15 13:01:59 -0700

    focusing the name input after a new entry is created, vastly simplifying data entry

--- js/grade-sheet.js
+++ js/grade-sheet.js
@@ -20,14 +20,30 @@ gradeSheet.service('PersistentStorageService', ['localStorageService',
         }
     }
 ]);
-gradeSheet.controller('GradeSheetCtrl', ['$scope', 'PersistentStorageService',
-    function($scope, PersistentStorageService) {
+// Credit goes to ryeballar on: http://stackoverflow.com/questions/25596399/set-element-focus-in-angular-way
+gradeSheet.factory('focus', function($timeout, $window) {
+    return function(id) {
+        // timeout makes sure that it is invoked after any other event has been triggered.
+        // e.g. click events that need to run before the focus or
+        // inputs elements that are in a disabled state but are enabled when those events
+        // are triggered.
+        $timeout(function() {
+            var element = $window.document.getElementById(id);
+            if (element) {
+                element.focus();
+            }
+        });
+    };
+});
+gradeSheet.controller('GradeSheetCtrl', ['$scope', 'focus', 'PersistentStorageService',
+    function($scope, focus, PersistentStorageService) {
         $scope._ = _;
         $scope.columnLabels = ['#', 'Name', 'Grade', 'Delete?'];
         $scope.entries = PersistentStorageService.getEntries();
 
         var watcher = $scope.$watch('entries', function(newEntries) {
                 PersistentStorageService.setEntries($scope.entries);
+                focus('thingy');
             },
             true); // Deep watch
 
